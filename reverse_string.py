5ea1d30607a2bc342d2d69b69d3024f60abb5da4
zasiddiq <zasiddiq@uci.edu>
2020-08-03 20:09:19 -0700

    updated comments

--- reverse_string.py
+++ reverse_string.py
@@ -4,10 +4,12 @@ def reverseString(s):
         """
         Do not return anything, modify s in-place instead.
         """
-        left, right = 0, len(s) - 1
+        left = 0
+        right = len(s) - 1
         while left < right:
             s[left], s[right] = s[right], s[left]
-            left, right = left + 1, right - 1
+            left += 1
+            right -= 1
         print("Outcome: ", s)
 
 if __name__ == '__main__':
