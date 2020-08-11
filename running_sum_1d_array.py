df337f3787a02cfcad89003bf4156e5064177300
zasiddiq <zasiddiq@uci.edu>
2020-08-10 21:53:33 -0700

    solved running_sum_1d_array.py

---
+++ running_sum_1d_array.py
@@ -0,0 +1,11 @@
+def running_sum(nums: list) -> list:
+    sum_list = []
+    sum = 0
+    for i in nums:
+        sum += i
+        sum_list.append(sum)
+    return sum_list
+
+if __name__ == '__main__':
+    print( running_sum([1,2,3,4]) ) # should be [1,3,6,10]
+    print( running_sum([1,1,1,1,1]) ) # should be [1,2,3,4,5]
